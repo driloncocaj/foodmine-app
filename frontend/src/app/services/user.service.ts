65c33153126ef4aeed14fc5b29bb65549fedfb06
Drilon Çoçaj <driloncoc@gmail.com>
2022-09-10 01:38:47 +0200

    Basic changes on Frontend and Backend

---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { USER_LOGIN_URL } from '../shared/constants/url';
+import { IUserLogin } from '../shared/interfaces/IUserLogin';
+import { User } from '../shared/models/User';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private userSubject = new BehaviorSubject<User>(new User());
+  public userObservable: Observable<User>;
+  constructor(private http: HttpClient) {
+    this.userObservable = this.userSubject.asObservable();
+  }
+
+  login(userLogin: IUserLogin): Observable<User> {
+    return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
+      tap({
+        next: (user) => {},
+        error: (errorRespose) => {},
+      })
+    );
+  }
+}
