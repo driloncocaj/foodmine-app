6e1337991c88b645f686cc5b128f0d7ab17abe54
Drilon Çoçaj <driloncoc@gmail.com>
2022-09-17 03:20:41 +0200

    Connected to MongoDB Atlas

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -1,22 +1,38 @@
 import { Router } from "express";
 import { sample_users } from "../data";
 import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import { UserModel } from "../models/user.model";
 const router = Router();
 
-//Login
-router.post("/login", (req, res) => {
-  //Destructuring Assignment
-  const { email, password } = req.body;
-  const user = sample_users.find(
-    (user) => user.email === email && user.password === password
-  );
+router.get(
+  "/seed",
+  asyncHandler(async (req, res) => {
+    const usersCount = await UserModel.countDocuments();
+    if (usersCount > 0) {
+      res.send("Seed is already done");
+      return;
+    }
+    await UserModel.create(sample_users);
+    res.send("Seed Is Done!");
+  })
+);
 
-  if (user) {
-    res.send(generateTokenResponse(user));
-  } else {
-    res.status(400).send("Username or password is not valid!");
-  }
-});
+//Login
+router.post(
+  "/login",
+  asyncHandler(async (req, res) => {
+    //Destructuring Assignment
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email, password });
+
+    if (user) {
+      res.send(generateTokenResponse(user));
+    } else {
+      res.status(400).send("Username or password is not valid!");
+    }
+  })
+);
 
 //Generating Token with JWT
 const generateTokenResponse = (user: any) => {
